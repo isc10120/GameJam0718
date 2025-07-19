3d07de15f74ed5c77f11a8ec5b45912b19a749cb
Ssomewhat <isc10120@gmail.com>
2025-07-20 08:27:14 +0900

    title scene, build settings

---
+++ Assets/Scripts/Opening.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Opening : MonoBehaviour
+{
+    [SerializeField] Button playButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButton.onClick.AddListener(StartGame);
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene("Eon");
+    }
+}
